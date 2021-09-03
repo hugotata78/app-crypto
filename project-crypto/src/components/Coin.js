d4b0edfb9f18a1c1f56b3a029874d7fd95a78cca
hugotata78 <hugotatarinoff@gmail.com>
2021-09-03 17:10:57 -0300

    mejoras

--- project-crypto/src/components/Coin.js
+++ project-crypto/src/components/Coin.js
@@ -1,31 +1,34 @@
+import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Coin = ({ coin }) => {
     return (
         <div className="card bg-dark mt-4 w-100 p-4 m-auto">
+            <Link to='/'>
+                <button className='btn btn-outline-primary btn-lg' title='Home'>
+                    <FontAwesomeIcon icon={faArrowAltCircleLeft}/>
+                </button>
+            </Link>
             <img src={coin.image && coin.image.large} className="w-25 mt-4 m-auto img-fluid" alt={coin.name} />
             <div className="card-body mt-4">
-            
+
                 <h1 className="card-title text-center">{coin.name}</h1>
                 <h3 className='mt-2'>Details</h3>
-                <p style={{ fontSize: '20px', margin:'0' }}>Symbol: <span>{coin.symbol}</span></p>
-                <p style={{ fontSize: '20px', margin:'0'  }}>Rank: <span>{coin.market_data && coin.market_data.market_cap_rank}</span></p>
-                <p style={{ fontSize: '20px', margin:'0'  }}>Price U$D: {coin.market_data && coin.market_data.current_price.usd}</p>
-                <p style={{ fontSize: '20px', margin:'0'  }}>
+                <p style={{ fontSize: '20px', margin: '0' }}>Symbol: <span>{coin.symbol}</span></p>
+                <p style={{ fontSize: '20px', margin: '0' }}>Rank: <span>{coin.market_data && coin.market_data.market_cap_rank}</span></p>
+                <p style={{ fontSize: '20px', margin: '0' }}>Price U$D: {coin.market_data && coin.market_data.current_price.usd}</p>
+                <p style={{ fontSize: '20px', margin: '0' }}>
                     Price Change U$D:{' '}
                     <span className={coin.market_data && coin.market_data.price_change_percentage_1h_in_currency.usd > 0 ? 'text-success' : 'text-danger'}>
                         {coin.market_data && coin.market_data.price_change_percentage_1h_in_currency.usd}
                     </span>
                 </p>
-                <p style={{ fontSize: '20px', margin:'0'  }}>Volume: <span>{coin.market_data && coin.market_data.total_volume.usd}</span></p>
+                <p style={{ fontSize: '20px', margin: '0' }}>Volume: <span>{coin.market_data && coin.market_data.total_volume.usd}</span></p>
                 <h3 className='mt-4'>Description: </h3>
                 <p >{coin.description && coin.description.es || 'No description'}</p>
-                
-                
-                
-                
-                
-                
+
             </div>
         </div>
     )
--- project-crypto/src/pages/ContainerListCoins.js
+++ project-crypto/src/pages/ContainerListCoins.js
@@ -2,28 +2,33 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import TableCoins from '../components/TableCoins';
 
+
 function ContainerListCoins() {
 
   const [coins, setCoins] = useState([])
   const [search, setSearch] = useState('')
+  const [data,setData] = useState(null)
+  const order = data || 'market_cap_desc'
   const getCoins = async () => {
-    const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=${order}&per_page=100&page=1&sparkline=false`)
     setCoins(response.data)
   }
+
   useEffect(() => {
     getCoins()
   }, [])
   return (
     <div className="container">
       <div className='row'>
+        <h1 className='mt-4 text-center'>Crypto Currency</h1>
         <input
           type='text'
           placeholder='Search ...'
-          className='form-control bg-dark text-light border-0 mt-4 text-center'
+          className='form-control bg-dark text-light border-0 mt-4 m-auto text-center'
           autoFocus
-          onChange={e=>setSearch(e.target.value)}
+          onChange={e => setSearch(e.target.value)}
         />
-        <TableCoins coins={coins} search={search}/>
+        <TableCoins coins={coins} search={search} />
       </div>
     </div>
   );
